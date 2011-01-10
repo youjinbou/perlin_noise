8d3696cbfe290713201cff4d570236da7d8f6e3c
Didier Cassirame <didier.cassirame@gmail.com>
2011-01-10 12:08:09 +0100

    * corrected copyright blurb in every ml files

--- README.txt
+++ README.txt
@@ -1,4 +1,9 @@
 This is a simple implementation of perlin noise.
+I only tested the 2D noise generator, but 1D, 3D, and N dimensional versions
+of the lib are also available. These are extrapolated to a certain extend from 
+the 2D generator code, so it might not behave as it should. I'll try to implement
+test programs for them later.
+
 
 released under the terms of the GPL license, see COPYING for details.
 
--- test_perlin_2d.ml
+++ test_perlin_2d.ml
@@ -1,3 +1,22 @@
+(*
+  a perlin noise library
+
+  Copyright (C) 2010  Didier Cassirame
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*)
 open Sdl;;
 open Sdlvideo;;
 open Sdlevent;;
@@ -9,8 +28,8 @@ module NConfig : (Noise.CONFIG) =
 struct 
   let interpolate = Interpolate.cubic
   let persistence = 1.0
-  let octaves     = 8
-  let psize       = 2048
+  let octaves     = 6
+  let psize       = 256
 end 
 
 module N = Noise.Make(NConfig)
@@ -28,7 +47,8 @@ module Texture_gen =
 struct
   type t = Sdlvideo.surface
       (*
-	let create w h = create_RGB_surface [ `ASYNCBLIT ; `HWSURFACE ] w h 32 0x000000FF 0x0000FF00 0x00FF0000 0xFF000000
+	let create w h = create_RGB_surface [ `ASYNCBLIT ; `HWSURFACE ] 
+	w h 32 0x000000FF 0x0000FF00 0x00FF0000 0xFF000000
       *)
   let put s (x,y) c = 
     put_pixel s ~x ~y (map_RGB s c)
